1a569afe5aa2195c327313f01c1c9d3b9219ebf0
mohamd haji <mohamdhaji0007@gmail.com>
2021-04-04 23:43:31 +0300

    cleaning unnecessary modules

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const multer = require("multer");
 const playerRoutes = require("./routes/player");
 const franchiseRoutes = require("./routes/franchise");
 
